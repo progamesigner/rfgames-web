c043f452a6319c3a9331ec56a2a01e9aa426e612
Yang Sheng Han <progamesigner@gmail.com>
2019-07-17 12:22:50 +0800

    Initial commit with basic files

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require("path");
+
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: ["./scripts/main.js", "./styles/main.scss"],
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          {
+            loader: "postcss-loader",
+            options: {
+              plugins: [
+                require("cssnano")({
+                  preset: [
+                    "default",
+                    {
+                      autoprefixer: {},
+                      discardComments: {
+                        removeAll: true
+                      }
+                    }
+                  ]
+                })
+              ]
+            }
+          },
+          {
+            loader: "sass-loader"
+          }
+        ]
+      }
+    ]
+  },
+  output: {
+    path: path.resolve(__dirname, "assets"),
+    filename: "[name].js"
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].css"
+    })
+  ]
+};
