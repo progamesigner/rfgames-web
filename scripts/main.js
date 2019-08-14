486cab036390e9273436c1367b3c7d4c1e0d6c81
Yang Sheng Han <progamesigner@gmail.com>
2019-08-14 14:39:42 +0800

    Add form submitting scripts and correctly transform form data to JSON payload

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,52 @@
 import { onLoaded as onImageLoaded } from './image'
+import onFormSubmit from './form'
 
 const bootstrap = () => {
+  document
+    .querySelectorAll('form[data-form="application"]')
+    .forEach(form => onFormSubmit(form, data => ({
+      account: data.get('account') || null,
+      discord: data.get('discord') || null,
+      age: data.get('age') === 'true',
+      goals: data.get('goals') === 'true',
+      times: data.get('times') === 'true',
+      microphone: data.get('microphone') === 'true',
+      commands: data.get('commands') === 'true',
+      main: data.get('main').toLowerCase(),
+      alt: data.get('alt').toLowerCase(),
+      message: data.get('message') || null
+    }), () => {
+      form.querySelectorAll('button[type="submit"]').forEach(button => {
+        button.classList.add('is-loading')
+        button.classList.remove('is-danger')
+        button.classList.remove('is-success')
+      })
+    }, success => {
+      form.querySelectorAll('button[type="submit"]').forEach(button => {
+        button.classList.add(success ? 'is-success' : 'is-danger')
+        button.classList.remove('is-loading')
+      })
+    }))
+
+  document
+    .querySelectorAll('form[data-form="contact"]')
+    .forEach(form => onFormSubmit(form, data => ({
+      name: data.get('account') || null,
+      email: data.get('discord') || null,
+      message: data.get('message') || null
+    }), () => {
+      form.querySelectorAll('button[type="submit"]').forEach(button => {
+        button.classList.add('is-loading')
+        button.classList.remove('is-danger')
+        button.classList.remove('is-success')
+      })
+    }, success => {
+      form.querySelectorAll('button[type="submit"]').forEach(button => {
+        button.classList.add(success ? 'is-success' : 'is-danger')
+        button.classList.remove('is-loading')
+      })
+    }))
+
   document.querySelectorAll('img[data-placeholder] ~ img').forEach(async image => {
     try {
       const { classList } = await onImageLoaded(image)
