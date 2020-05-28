c17c5b5efda6b6ecdeabd42bcb7bf3944396a350
Yang Sheng Han <progamesigner@gmail.com>
2020-05-28 17:29:14 +0800

    Add devtool and move chunks into folder to be easily managed

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,12 @@
 const path = require('path')
 
-const ManifestPlugin = require('webpack-assets-manifest')
+const ManifestPlugin = require('webpack-manifest-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
+const isProduction = process.env.NODE_ENV === 'production'
+
 module.exports = {
+  devtool: isProduction ? false : 'eval-source-map',
   entry: [
     './scripts/main.js',
     './styles/main.scss'
@@ -22,21 +25,19 @@ module.exports = {
     ]
   },
   output: {
-    chunkFilename: '[chunkhash].chunk.js',
+    chunkFilename: '.chunks/[chunkhash].chunk.js',
     filename: '[name].js',
     jsonpFunction: 'rfgamesJsonp', // @note: to avoid conflict with gw2-embed
     path: path.resolve(__dirname, 'assets'),
     publicPath: '/'
   },
   plugins: [
-    new ManifestPlugin({
-      integrity: true,
-      integrityHashes: ['sha512'],
-      output: path.join(path.resolve(__dirname, 'data'), 'assets.json'),
-      publicPath: false
-    }),
     new MiniCssExtractPlugin({
       filename: '[name].css'
+    }),
+    new ManifestPlugin({
+      fileName: path.join(path.resolve(__dirname, 'data'), 'assets.json'),
+      publicPath: false
     })
   ]
 }
