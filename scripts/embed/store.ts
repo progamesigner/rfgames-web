2effe8c13151e112e6671cec42559acff568e72d
Yang Sheng Han <progamesigner@gmail.com>
2020-06-03 01:06:51 +0800

    Move option parsing to store creation

--- scripts/embed/store.ts
+++ scripts/embed/store.ts
@@ -1,9 +1,18 @@
-import { createStore, Store } from 'redux'
+import { Action, createStore, Store } from 'redux'
 
-import { default as reducer } from './reducers'
-import { default as initialState } from './states'
-import { EmbedOptions } from './types'
+import { default as rootReducer } from './reducers'
+import { default as initializeState } from './states'
+import { EmbedOptions, EmbedState } from './types'
 
-export default function(options: EmbedOptions): Store {
-  return createStore(reducer, initialState(options))
+function parseOptions(window: Window): EmbedOptions {
+  return {
+    cacheVersion: '__INITIAL__',
+    language: 'en',
+    useLocalStorageAsCache: true,
+    ...window.GW2_EMBED_OPTIONS
+  }
+}
+
+export default function(window: Window): Store<EmbedState, Action> {
+  return createStore(rootReducer, initializeState(parseOptions(window)))
 }
