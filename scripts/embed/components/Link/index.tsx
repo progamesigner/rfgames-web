9302bdbde25cf16ed101effaa734b4b2f003d361
Yang Sheng Han <progamesigner@gmail.com>
2020-06-03 13:47:51 +0800

    Add Link component

---
+++ scripts/embed/components/Link/index.tsx
@@ -0,0 +1,31 @@
+import * as m from 'mithril'
+
+import { cx, makeClassName } from '../../libs'
+
+import * as styles from './styles'
+
+export type LinkAttributes = m.Attributes & {
+  href?: string;
+}
+
+export default class implements m.Component<LinkAttributes> {
+  public view({
+    attrs: {
+      className,
+      ...attrs
+    },
+    children
+  }: m.Vnode<LinkAttributes>): m.Children {
+    const {
+      href
+    } = attrs
+
+    return href ? <a
+      className={cx(styles.root, makeClassName('link'), className)}
+      href={href}
+      rel="noopener noreferrer"
+      target="_blank"
+      {...attrs}
+    >{children}</a> : children
+  }
+}
---
+++ scripts/embed/components/Link/styles.ts
@@ -0,0 +1,3 @@
+import { style } from '../../libs'
+
+export const root = style({})
