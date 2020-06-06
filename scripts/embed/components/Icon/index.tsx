b039753ff6b8ba43f847165d11b8d88f21b5b7e3
Yang Sheng Han <progamesigner@gmail.com>
2020-06-06 12:30:55 +0800

    Improve Icon component

--- scripts/embed/components/Icon/index.tsx
+++ scripts/embed/components/Icon/index.tsx
@@ -4,22 +4,36 @@ import { cx, makeClassName } from '../../libs'
 
 import * as styles from './styles'
 
+const COUNT_THRESHOLD = 1
+
 interface IconAttributes extends m.Attributes {
-  src: string;
+  applyCount?: number;
+  classSize?: string;
+  src?: string;
 }
 
 export class Icon implements m.Component<IconAttributes> {
   public view({
     attrs: {
+      applyCount,
       className,
+      classSize,
       src,
       ...attrs
     },
     children
   }: m.Vnode<IconAttributes>): m.Children {
     return <div
-      className={cx(styles.root, styles.image(src), makeClassName('icon'), className)}
+      className={cx(styles.root, classSize, makeClassName('icon'), className)}
       {...attrs}
-    >{children}</div>
+    >
+      <div className={cx(styles.icon, src && styles.image(src))}></div>
+      {
+        applyCount && applyCount > COUNT_THRESHOLD ?
+        <span className={styles.applyCount}>{applyCount}</span> :
+        null
+      }
+      {children}
+    </div>
   }
 }
--- scripts/embed/components/Icon/styles.ts
+++ scripts/embed/components/Icon/styles.ts
@@ -1,12 +1,44 @@
-import { rem, style, url } from '../../libs'
+import { border, color, percent, px, rem, style, url } from '../../libs'
 
 export const root = style({
-  backgroundSize: 'cover',
+  backgroundColor: color('#151718').toString(),
+  backgroundImage: `radial-gradient(circle, ${color('#606468').toString()}, ${color('#333333').toString()})`,
+  border: border({
+    color: color('#151718').darken(percent(25)).toString(),
+    style: 'solid',
+    width: px(1)
+  }),
   display: 'inline-block',
   height: rem(4),
+  pointerEvents: 'none',
+  position: 'relative',
   width: rem(4)
+})
+
+export const icon = style({
+  backgroundPosition: 'center',
+  backgroundRepeat: 'no-repeat',
+  backgroundSize: 'cover',
+  bottom: 0,
+  left: 0,
+  position: 'absolute',
+  right: 0,
+  top: 0
 })
 
 export const image = (src: string): string => style({
   backgroundImage: url(src)
+})
+
+export const applyCount = style({
+  bottom: rem(0.25),
+  color: color('#fff4cf').toString(),
+  fontSize: rem(0.75),
+  margin: 0, // @note: force reset style for compatibility
+  padding: 0, // @note: force reset style for compatibility
+  pointerEvents: 'none',
+  position: 'absolute',
+  right: rem(0.25),
+  textShadow: `0 0 ${px(5)} ${color('#151718').toString()}`,
+  zIndex: 100
 })
