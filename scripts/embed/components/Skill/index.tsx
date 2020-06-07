2f1ccc9f7a11b6e55ba4eb19c53dd0e56bff5d36
Yang Sheng Han <progamesigner@gmail.com>
2020-06-08 03:05:06 +0800

    Update skill component

--- scripts/embed/components/Skill/index.tsx
+++ scripts/embed/components/Skill/index.tsx
@@ -1,38 +1,78 @@
 import * as m from 'mithril'
 
-import { cx } from '../../libs'
 import {
   GW2Skill,
-  HasIDAttributes
+  HasIDAttributes,
+  HasRenderAttributes,
+  HasStoreAttributes,
+  HasTooltipAttributes,
+  TooltipType
 } from '../../types'
 
-import { buildWikiLink } from '../helpers'
-
+import { Container } from '../Container'
 import { Icon } from '../Icon'
 import { Link } from '../Link'
+import { Name } from '../Name'
+
+import { bindTooltipEvents, buildWikiLink } from '../helpers'
+
+import './Tooltip'
 
 import * as styles from './styles'
 
-type SkillAttributes = m.Attributes & HasIDAttributes<number> & GW2Skill
+interface SkillAttributes extends
+  m.Attributes,
+  HasIDAttributes<number>,
+  HasRenderAttributes,
+  HasStoreAttributes,
+  HasTooltipAttributes
+{
+  data: GW2Skill;
+}
 
 export class Skill implements m.Component<SkillAttributes> {
   public view({
-    attrs: data
+    attrs: {
+      data,
+      disableIcon,
+      disableLink,
+      disableText,
+      disableTooltip,
+      inline,
+      store
+    }
   }: m.Vnode<SkillAttributes>): m.Children {
     const {
       icon,
-      name,
-      slot
+      name
     } = data
 
-    return <div className={cx(styles.root)}>
-      <Icon
-        className={cx(styles.icon, 'is-skill', `is-${slot.toLowerCase()}`)}
-        src={icon}
-      />
-      <span className={cx(styles.name)}>
-        <Link href={buildWikiLink(name)}>{name}</Link>
-      </span>
-    </div>
+    const tooltipEvents = !disableTooltip ?
+      bindTooltipEvents(store, TooltipType.GW2_SKILL, data) :
+      {}
+
+    return <Container inline={!disableText || inline} type="skill">
+      {
+        !disableIcon ?
+        <Icon
+          src={icon}
+          inline={!disableText || inline}
+          className={styles.icon}
+          {...tooltipEvents}
+        /> :
+        null
+      }
+      {
+        !disableText ?
+        <Name className={styles.name} {...tooltipEvents}>
+          {
+            !disableLink ?
+            <Link className={styles.link} href={buildWikiLink(name)}>{name}</Link> :
+            name
+          }
+        </Name> :
+        null
+      }
+    </Container>
   }
 }
