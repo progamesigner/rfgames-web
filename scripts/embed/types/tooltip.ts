86db0d64a626d7aa93eb8c6f15073373de20da05
Yang Sheng Han <progamesigner@gmail.com>
2020-06-08 17:53:24 +0800

    Reorder export types

--- scripts/embed/types/tooltip.ts
+++ scripts/embed/types/tooltip.ts
@@ -6,11 +6,11 @@ export interface TooltipTypeMapping {
   [TooltipType.EMPTY]: [null, null];
 }
 
-export type ExtractTooltipAttributeType<T extends TooltipType> = TooltipTypeMapping extends Pick<TooltipTypeMapping, T> ? TooltipTypeMapping[T][0] : never
-export type ExtractTooltipDataType<T extends TooltipType> = TooltipTypeMapping extends Pick<TooltipTypeMapping, T> ? TooltipTypeMapping[T][1] : never
-
 export interface TooltipState {
   data: ExtractTooltipDataType<TooltipType>;
   show: boolean;
   type: TooltipType;
 }
+
+export type ExtractTooltipAttributeType<T extends TooltipType> = TooltipTypeMapping extends Pick<TooltipTypeMapping, T> ? TooltipTypeMapping[T][0] : never
+export type ExtractTooltipDataType<T extends TooltipType> = TooltipTypeMapping extends Pick<TooltipTypeMapping, T> ? TooltipTypeMapping[T][1] : never
