72a9f9c9e65fc5b883b376c228926f1347c355bd
Yang Sheng Han <progamesigner@gmail.com>
2020-06-15 14:13:18 +0800

    Export coin images with GW2 render service

---
+++ scripts/embed/images/coins.ts
@@ -0,0 +1,3 @@
+export const gold = 'https://render.guildwars2.com/file/090A980A96D39FD36FBB004903644C6DBEFB1FFB/156904.png'
+export const silver = 'https://render.guildwars2.com/file/E5A2197D78ECE4AE0349C8B3710D033D22DB0DA6/156907.png'
+export const copper = 'https://render.guildwars2.com/file/6CF8F96A3299CFC75D5CC90617C3C70331A1EF0E/156902.png'
--- scripts/embed/images/index.ts
+++ scripts/embed/images/index.ts
@@ -1,3 +1,1 @@
-export { default as copper } from './copper.png'
-export { default as gold } from './gold.png'
-export { default as silver } from './silver.png'
+export * from './coins'
