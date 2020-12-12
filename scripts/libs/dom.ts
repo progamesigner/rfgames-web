867988f9d31d2ad1e8c1b1d097505c208db1f942
Yang Sheng Han <progamesigner@gmail.com>
2020-12-12 14:46:27 +0800

    Implement scroll event helpers in dom library

--- scripts/libs/dom.ts
+++ scripts/libs/dom.ts
@@ -1,4 +1,19 @@
+import { debounce, throttle } from 'lodash/fp'
+
+type ScrollEventCallback = (x: number, y: number) => void
+
 export type UnbindEventListener = () => void
+
+let scrollEventInstalled = false
+let scrollEventCallbacks = [] as Array<ScrollEventCallback>
+
+export function currentScrollX(): number {
+  return window.scrollX || window.pageXOffset || 0
+}
+
+export function currentScrollY(): number {
+  return window.scrollY || window.pageYOffset || 0
+}
 
 export function bindEventListener(
   element: Document | Element | Window,
@@ -7,6 +22,29 @@ export function bindEventListener(
 ): UnbindEventListener {
   element.addEventListener(event, listener, false)
   return (): void => element.removeEventListener(event, listener, false)
+}
+
+export function bindScrollEvent(
+  window: Window,
+  callback: ScrollEventCallback
+): void {
+  scrollEventCallbacks = [...scrollEventCallbacks, callback]
+
+  if (!scrollEventInstalled) {
+    const onScrollEvent = () => {
+        const x = currentScrollX()
+        const y = currentScrollY()
+        scrollEventCallbacks.forEach(callback => callback(x, y))
+    }
+
+    window.addEventListener('scroll', throttle(100)(onScrollEvent))
+    window.addEventListener('resize', debounce(50)(onScrollEvent))
+    window.addEventListener('orientationchange', debounce(50)(onScrollEvent))
+
+    scrollEventInstalled = true
+  }
+
+  callback(currentScrollX(), currentScrollY())
 }
 
 export function loadScript(window: Window, url: string): void {
